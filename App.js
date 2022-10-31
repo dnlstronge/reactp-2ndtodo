124a977260d861bc7e37de5bf2e33d3b9241ff8b
Dan <114242578+dnlstronge@users.noreply.github.com>
2022-10-31 11:05:28 +0000

    Create App.js
    import from CP (started)
    needs imports added

---
+++ App.js
@@ -0,0 +1,11 @@
+function App () {
+    return (
+    <>
+    <div id="do"> My To Do list </div>
+    <input type="text"/> 
+    <button>Add item</button>
+            <button>Clear Item</button>
+    </>)         
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
